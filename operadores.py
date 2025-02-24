83d58317df9c3fbb2ede9fb5ead790ef5a746bb6
Sebastian <tequiasebastian460@gmail.com>
2025-02-24 09:11:10 -1200

    mi primera expresion en python

--- operadores.py
+++ operadores.py
@@ -5,3 +5,15 @@
 # sigue la jerarquia de operadores
 # establecida en la mayoría de los 
 # leguajes de programación
+
+# 1. Operadores aritméticos: 
+# +, - , * , / , % , **
+
+a = 5
+b = 2
+c = 4
+d = 3
+
+y = a ** b - c / d + 1
+
+print(y)
