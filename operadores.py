7b35d7e3e7c6e2f963156cdf88de7eb07d266e3a
Sebastian <tequiasebastian460@gmail.com>
2025-02-24 07:54:05 -1200

    se creo el archivo operadores.py

---
+++ operadores.py
@@ -0,0 +1,7 @@
+## JERARQUIA DE OPERADORES 
+# En Python
+# -----------------------
+#La instrucciones en python
+# sigue la jerarquia de operadores
+# establecida en la mayoría de los 
+# leguajes de programación
